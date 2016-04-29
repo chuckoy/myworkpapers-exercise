28877da07840b4c0797146cc88c2b4472042fd71
Chuck Logan Lim <chucklogan.lim@gmail.com>
2016-04-29 21:57:43 +1000

    Add folder

---
+++ folder/config/secrets.php
@@ -0,0 +1,6 @@
+$config['displayErrorDetails'] = true;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "root";
+$config['db']['pass']   = "password";
+$config['db']['dbname'] = "myworkpapers";
